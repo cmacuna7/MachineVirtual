3c87ddcd95aecb8c233c1b4a219afffde44e407a
cmacuna7 <cmacuna@espe.edu.ec>
2024-12-21 16:28:24 -0500

    Proyecto

--- ProyectoPrimerParcial/main.cpp
+++ ProyectoPrimerParcial/main.cpp
@@ -3,7 +3,7 @@
  * Proposito:                      Archivo principal de proyecto                            *
  * Autor:                          Abner Arboleda, Christian Acuña, Christian Bonifaz       *
  * Fecha de creacion:              01/12/2024                                               *
- * Fecha de modificacion:          21/12/2024                                               *
+ * Fecha de modificacion:          08/11/2024                                               *
  * Materia:                        Estructura de datos                                      *
  * NRC :                           1992                                                     *
  ********************************************************************************************/
